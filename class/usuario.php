e081f9fdce78b7be698c55b49c00bb8290ced179
Daniel do Nascimento <danielnasc2012@hotmail.com>
2020-05-25 19:54:42 -0300

    Atualizando 25-05 list-group busch

--- class/usuario.php
+++ class/usuario.php
@@ -69,6 +69,43 @@ class Usuario {
 
     }
 
+    //ELE LISTA TODOS QUE ESTÃO NA TABELA tb_usuario
+    public static function getList(){
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuario ORDER BY deslogin;");
+    }
+
+    //ELE TRAS UMA LISTA DE BUSCA BASEADO PELO LOGIN
+    public static function search($login){
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_usuario where deslogin LIKE :SEARCH ORDER BY deslogin", array(
+            ':SEARCH'=>"%".$login."%"
+        ));
+    }
+
+    //
+    public function login($login,$password){
+        $sql = new Sql();
+        $results = $sql->select("SELECT * FROM tb_usuario WHERE deslogin = :LOGIN AND  dessenha = :PASSWORD ",array(
+            ":LOGIN"=>$login, 
+            ":PASSWORD"=>$password
+        ));
+
+        if(count($results)>0) {
+
+            $row = $results[0];
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+        }
+        else {
+            throw new Exception("Login e/ou senha invalidos.");
+        }
+    }
+    
+
 
     public function __toString(){
         return json_encode(array(
--- index.php
+++ index.php
@@ -1,26 +1,38 @@
 <?php
 require_once("config.php");
 
+/*
+//CARREGA UMA LISTA DE USUARIO DE BUSCA
+$search = Usuario::search("a");
+echo json_encode($search);
+*/
 
-$root = new Usuario();
+$usuario =new Usuario();
 
-$root->loadbyId(2);
+$usuario->login("Robson","987654");
 
-echo $root;
+echo $usuario;
 
-
-
-
+/*
+//TRAS UMA LISTA DE USUARIOS
+$lista = Usuario::getList();
+echo json_encode($lista);
+*/
 
 
 
 /*
+//AQUI SÓ TRAS A INFORMAÇÃO DE UM USUARIO
+$root = new Usuario();
+$root->loadbyId(2);
+echo $root;
+*/
+
+
+/*
 $sql = new Sql();
-
 $usuarios = $sql->select("SELECT * FROM tb_usuario");
-
 echo json_encode($usuarios);
-
 */
 
 ?>
